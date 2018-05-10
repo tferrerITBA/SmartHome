aeac6fcf37437f19dd2019d735b09a0fbea84b8a
Unknown <mlund@itba.edu.ar>
2018-05-10 16:51:12 -0300

    Order style
    Added cancel button, fixed createDevice.html and style.css

--- vue.js
+++ vue.js
@@ -1,6 +1,19 @@
+Vue.component('devices', {
+
+})
+
 var app = new Vue({
 	el: '#app',
 	data: {
-		devices: 0
+		deviceQty: 0,
+		deviceList: []
+	},
+	methods: {
+		addDevice: function() {
+			var deviceName = document.getElementById("deviceName").value;
+			var deviceType = document.getElementById("deviceType").value;
+			this.deviceList.push({ name: deviceName, type: deviceType });
+			this.deviceQty++;
+		}
 	}
 })

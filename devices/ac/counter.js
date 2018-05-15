7db33d7dbf227b295c1c8a8e8dd663ac12451d99
Unknown <mlund@itba.edu.ar>
2018-05-14 21:56:01 -0300

    Deleted search bar, search files and switch folder

---
+++ devices/ac/counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $("#plus").click(function() {
+        var currentVal = parseInt($('input[name=quantity]').val());
+        if (currentVal < 25) {
+            $('input[name=quantity]').val(currentVal + 1);
+        } else {
+            $('input[name=quantity]').val(25);
+        }
+    });
+
+    $("#minus").click(function() {
+        var currentVal = parseInt($('input[name=quantity]').val());
+        if (currentVal > 15) {
+            $('input[name=quantity]').val(currentVal - 1);
+        } else {
+            $('input[name=quantity]').val(15);
+        }
+    });
+});

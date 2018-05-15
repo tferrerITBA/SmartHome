76e94a43e5b468726b9b256cdb40160710661624
gprinc <gprinc@itba.edu.ar>
2018-05-15 17:37:20 -0300

    lamp and oven state

--- devices/oven/oven.js
+++ devices/oven/oven.js
@@ -2,6 +2,32 @@ $(document).ready(function() {
 
   //$(".title-text").text(device.name);
 
+  api.device.executeAction(device.id, "getState", [])
+    .done((data, textStatus, jqXHR) => {
+      var result = data.result;
+      if (result.status === "on") {
+        $("#on-status").text("On");
+        $("#on-switch").prop("checked", true);
+      } else {
+        $("#on-status").text("Off");
+      }
+      $('input[name=quantity]').val(result.temperature);
+      $("#" + result.mode).prop("checked", true);
+      $("#vSwing" + result.verticalSwing).prop("checked", true);
+      $("#hSwing" + result.horizontalSwing).prop("checked", true);
+      $("#fanSpeed" + result.fanSpeed).prop("checked", true);
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+      console.log(jqXHR.responseText);
+      $("#on-status").text("Off");
+      $('input[name=quantity]').text("24");
+      $("#cool").prop("checked", true);
+      $("#vSwingauto").prop("checked", true);
+      $("#hSwingauto").prop("checked", true);
+      $("#fanSpeedauto").prop("checked", true);
+    })
+    
+
   $("#on-switch").on("click", function() {
     var status = $("#on-status").text()
     if(status === "On") {

b9c1f2cbe46452c707df85db8124c6bd5bc40d4e
Unknown <mlund@itba.edu.ar>
2018-05-14 21:46:12 -0300

    Added door behaviour and modularized css files

--- devices/counter.js
+++ devices/counter.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
 
     $("#plus").click(function() {
         var currentVal = parseInt($('input[name=quantity]').val());
---
+++ devices/door/door.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  //device.name
+
+  $("#open-switch").on("click", function() {
+    var status = $("#open-status").text()
+    if(status === "Open") {
+      //api.device.executeAction(device.id, "close")
+      //.done((data, textStatus, jqXHR) => {
+        $("#open-status").text("Closed");
+      //})
+      //.fail((jqXHR, textStatus, errorThrown) => {
+      //  console.log(jqXHR.responseText);
+      //})
+    } else {
+      //api.device.executeAction(device.id, "open")
+      //.done((data, textStatus, jqXHR) => {
+        $("#open-status").text("Open");
+      //})
+      //.fail((jqXHR, textStatus, errorThrown) => {
+      //  console.log(jqXHR.responseText);
+      //})
+    }
+  });
+
+  $("#lock-switch").on("click", function() {
+    var status = $("#lock-status").text()
+    if(status === "Locked") {
+      //api.device.executeAction(device.id, "unlock")
+      //.done((data, textStatus, jqXHR) => {
+        $("#lock-status").text("Unlocked");
+      //})
+      //.fail((jqXHR, textStatus, errorThrown) => {
+      //  console.log(jqXHR.responseText);
+      //})
+    } else {
+      //api.device.executeAction(device.id, "lock")
+      //.done((data, textStatus, jqXHR) => {
+        $("#lock-status").text("Locked");
+      //})
+      //.fail((jqXHR, textStatus, errorThrown) => {
+      //  console.log(jqXHR.responseText);
+      //})
+    }
+  });
+});

44c83dc1643e9828fcb5c1ae8aee8e7d035d1500
Unknown <mlund@itba.edu.ar>
2018-05-16 20:57:34 -0300

    Fixing merge

--- components.js
+++ components.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Vue.component('item', {props: ['name', 'subtext', 'img', 'id'],
 template: 
 	`<button v-bind:id="id" class="device" type="button">
@@ -22,27 +21,3 @@ var VueInstance = new Vue({
 		}
 	}
 })
-=======
-Vue.component('item', {props: ['name', 'subtext', 'img', 'id'],
-template: 
-	`<button v-bind:id="id" class="device" type="button" onclick=" ">
-					<div class="item-description">
-						<img class="item-icon" v-bind:src="img" alt="device"/>
-						<div class="item-text">
-							<p class="item-name">{{name}}</p>
-							<p class="item-subtext">{{subtext}}</p>
-						</div>
-					</div>
-				</button>`})
-
-var vueInstance = new Vue({
-	el: '.instances',
-	data: {	
-	}
-})
-
-// $(devices.html).ready(function hola(){
-// 	var items = $('<p>safsdf</p>');
-// 	$('.instances').append(items);
-// })
->>>>>>> master

334fc7dd75664a59ac9b85736eb98b6c77ac7223
gprinc <gprinc@itba.edu.ar>
2018-05-19 14:16:14 -0300

    device to room

---
+++ roomForDevice/roomForDevice.js
@@ -0,0 +1,43 @@
+var device;
+
+$(document).ready(function() {
+  api.room.getAll()
+    .done(function(data, textStatus, jqXHR) {
+      data.rooms.forEach(function(room){
+        var name = room.name;
+        var icon = room.meta.icon;
+        var id = room.id;
+        debugger;
+        VueInstance.addDevice(name, "", icon, id);
+      })
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR.responseText);
+    })
+
+  $(".instances").on("click", "button", function() {
+    console.log($(this).attr('id'));
+    var id = $(this).attr('id');
+      api.device.get($(this).attr('id'))
+      .done(function(data, textStatus, jqXHR) {
+        device = data.device;
+        var queryString = "?id=" + device.id;
+        if(device.typeId === acId) {
+          window.location.href = "devices/ac/ac.html" + queryString;
+        } else if(device.typeId === blindId) {
+          window.location.href = "devices/blinds/blinds.html" + queryString;
+        } else if(device.typeId === doorId) {
+          window.location.href = "devices/door/door.html" + queryString;
+        } else if(device.typeId === lampId) {
+          window.location.href = "devices/lamp/lamp.html" + queryString;
+        } else if (device.typeId === ovenId) {
+          window.location.href = "devices/oven/oven.html" + queryString;
+        } else if (device.typeId === refriId) {
+          window.location.href = "devices/refrigerator/refrigerator.html" + queryString;
+        }
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR.responseText);
+      });
+  });
+});

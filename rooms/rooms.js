d1b7e2f6dae45b2d7651a804d71e6cf243c7fe89
gprinc <gprinc@itba.edu.ar>
2018-05-21 20:40:07 -0300

    room view start

--- rooms/rooms.js
+++ rooms/rooms.js
@@ -1,13 +1,17 @@
-var room;
-
 $(document).ready(function() {
   api.room.getAll()
     .done(function(data, textStatus, jqXHR) {
       data.rooms.forEach(function(room){
-        var name = room.name;
-        var icon = room.meta.split(" ")[2];
-        var id = room.id;
-        VueInstance.addItem(name, "", icon, id);
+        api.room.getDevices(room.id)
+          .done(function(data, textStatus, jqXHR) {
+            var name = room.name;
+            var icon = room.meta.split(" ")[2];
+            var id = room.id;
+            VueInstance.addItem(name, "Devices: " + data.devices.length , icon, id);
+          })
+          .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.responseText);
+          })
       })
     })
     .fail(function(jqXHR, textStatus, errorThrown) {

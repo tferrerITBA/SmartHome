cccdc2ed7eee36d15854d8ffd9a203ccfb303b36
gprinc <gprinc@itba.edu.ar>
2018-05-21 18:58:57 -0300

    room for devices

--- roomForDevice/roomForDevice.js
+++ roomForDevice/roomForDevice.js
@@ -1,5 +1,9 @@
 var device;
 
+var queryString = decodeURIComponent(window.location.search);
+queryString = queryString.substring(1);
+var queries = queryString.split("=");
+
 $(document).ready(function() {
   api.room.getAll()
     .done(function(data, textStatus, jqXHR) {
@@ -7,7 +11,6 @@ $(document).ready(function() {
         var name = room.name;
         var icon = room.meta.icon;
         var id = room.id;
-        debugger;
         VueInstance.addDevice(name, "", icon, id);
       })
     })
@@ -16,28 +19,33 @@ $(document).ready(function() {
     })
 
   $(".instances").on("click", "button", function() {
-    console.log($(this).attr('id'));
     var id = $(this).attr('id');
-      api.device.get($(this).attr('id'))
-      .done(function(data, textStatus, jqXHR) {
-        device = data.device;
-        var queryString = "?id=" + device.id;
-        if(device.typeId === acId) {
-          window.location.href = "devices/ac/ac.html" + queryString;
-        } else if(device.typeId === blindId) {
-          window.location.href = "devices/blinds/blinds.html" + queryString;
-        } else if(device.typeId === doorId) {
-          window.location.href = "devices/door/door.html" + queryString;
-        } else if(device.typeId === lampId) {
-          window.location.href = "devices/lamp/lamp.html" + queryString;
-        } else if (device.typeId === ovenId) {
-          window.location.href = "devices/oven/oven.html" + queryString;
-        } else if (device.typeId === refriId) {
-          window.location.href = "devices/refrigerator/refrigerator.html" + queryString;
-        }
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR.responseText);
-      });
+      api.device.addDeviceToRoom(queries[1], id)
+        .done(function(data, textStatus, jqXHR) {
+          api.device.get(queries[1])
+            .done(function(data, textStatus, jqXHR) {
+              device = data.device;
+              var queryString = "?id=" + device.id;
+              if(device.typeId === acId) {
+                window.location.href = "../devices/ac/ac.html" + queryString;
+              } else if(device.typeId === blindId) {
+                window.location.href = "../devices/blinds/blinds.html" + queryString;
+              } else if(device.typeId === doorId) {
+                window.location.href = "../devices/door/door.html" + queryString;
+              } else if(device.typeId === lampId) {
+                window.location.href = "../devices/lamp/lamp.html" + queryString;
+              } else if (device.typeId === ovenId) {
+                window.location.href = "../devices/oven/oven.html" + queryString;
+              } else if (device.typeId === refriId) {
+                window.location.href = "../devices/refrigerator/refrigerator.html" + queryString;
+              }
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+              console.log(jqXHR.responseText);
+            });
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR.responseText);
+        });
   });
 });

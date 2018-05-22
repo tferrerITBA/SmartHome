92e2deed6e49fb7c82797ca03708668924510947
tferrerITBA <tferrer@itba.edu.ar>
2018-05-22 18:59:21 -0300

    Implements input validation.

--- createDevice/createDevice.js
+++ createDevice/createDevice.js
@@ -18,6 +18,23 @@ $(document).ready(function() {
           })
           .fail(function(jqXHR, textStatus, errorThrown) {
             console.log(jqXHR.responseText);
+            var str = JSON.stringify(jqXHR.responseText, null, 2);
+            var errorMessage = "";
+            str = str.replace(/(?:\\[rn])+/g, "");
+            str = str.replace(/[^\w\s]/gi, '');
+            if(str.indexOf('name is required') >= 0) {
+              errorMessage = errorMessage.concat("A name is required; ");
+            }
+            if(str.indexOf('fails to match the required pattern') >= 0) {
+              errorMessage = errorMessage.concat("Enter alphanumeric characters only; ");
+            }
+            if(str.indexOf('length must be') >= 0) {
+              errorMessage = errorMessage.concat("Please enter between 3 and 60 characters; ");
+            }
+            if(errorMessage != "")
+              $("#input-error").text(errorMessage);
+            else
+              $("#input-error").text(str);
           })
       })
       .fail(function(jqXHR, textStatus, errorThrown) {
--- devices/modifyDevice.js
+++ devices/modifyDevice.js
@@ -43,36 +43,48 @@ $(document).ready(function() {
 	        .done(function(data, textStatus, jqXHR) {
 	        	device = data.device;
 	        	var name = $("#name").val();;
-    			if (name !== "") {
-      				device.name = name;
-      				api.device.modify(device, device.id)
-      					.done(function(data, textStatus, jqXHR) {
-      						console.log("OK");
-        					var queryString = "?id=" + device.id;
-				          	if(device.typeId === acId) {
-				            	window.location.href = "ac/ac.html" + queryString;
-				          	} else if(device.typeId === blindId) {
-				         		window.location.href = "blinds/blinds.html" + queryString;
-				          	} else if(device.typeId === doorId) {
-				            	window.location.href = "door/door.html" + queryString;
-				          	} else if(device.typeId === lampId) {
-				            	window.location.href = "lamp/lamp.html" + queryString;
-				          	} else if (device.typeId === ovenId) {
-				            	window.location.href = "oven/oven.html" + queryString;
-				          	} else if (device.typeId === refriId) {
-				            	window.location.href = "refrigerator/refrigerator.html" + queryString;
-				          	}
-	        			})
-	        			.fail(function(jqXHR, textStatus, errorThrown) {
-	        				console.log("FALLA");
-	          				console.log(jqXHR.responseText);
-	        			})
-      			} else {
-      				// Mostrar error
-      			}
-      		})
-      		.fail(function(jqXHR, textStatus, errorThrown) {
-        		console.log(jqXHR.responseText);
-      		})	          	
+				device.name = name;
+				api.device.modify(device, device.id)
+					.done(function(data, textStatus, jqXHR) {
+						console.log("OK");
+						var queryString = "?id=" + device.id;
+						if(device.typeId === acId) {
+							window.location.href = "ac/ac.html" + queryString;
+						} else if(device.typeId === blindId) {
+							window.location.href = "blinds/blinds.html" + queryString;
+						} else if(device.typeId === doorId) {
+							window.location.href = "door/door.html" + queryString;
+						} else if(device.typeId === lampId) {
+							window.location.href = "lamp/lamp.html" + queryString;
+						} else if (device.typeId === ovenId) {
+							window.location.href = "oven/oven.html" + queryString;
+						} else if (device.typeId === refriId) {
+							window.location.href = "refrigerator/refrigerator.html" + queryString;
+						}
+					})
+					.fail(function(jqXHR, textStatus, errorThrown) {
+						console.log(jqXHR.responseText);
+						var str = JSON.stringify(jqXHR.responseText, null, 2);
+						var errorMessage = "";
+						str = str.replace(/(?:\\[rn])+/g, "");
+						str = str.replace(/[^\w\s]/gi, '');
+						if(str.indexOf('name is required') >= 0) {
+						errorMessage = errorMessage.concat("A name is required; ");
+						}
+						if(str.indexOf('fails to match the required pattern') >= 0) {
+						errorMessage = errorMessage.concat("Enter alphanumeric characters only; ");
+						}
+						if(str.indexOf('length must be') >= 0) {
+						errorMessage = errorMessage.concat("Please enter between 3 and 60 characters; ");
+						}
+						if(errorMessage != "")
+							$("#input-error").text(errorMessage);
+						else
+							$("#input-error").text(str);
+									})
+						})
+				.fail(function(jqXHR, textStatus, errorThrown) {
+					console.log(jqXHR.responseText);
+				})	          	
     });
 });

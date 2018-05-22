8ba90c45129f4e72d69835250cc6e2fcc0631116
Unknown <mlund@itba.edu.ar>
2018-05-21 23:17:57 -0300

    Started Room page

--- roomForDevice/roomForDevice.js
+++ roomForDevice/roomForDevice.js
@@ -14,10 +14,16 @@ $(document).ready(function() {
   api.room.getAll()
     .done(function(data, textStatus, jqXHR) {
       data.rooms.forEach(function(room){
-        var name = room.name;
-        var icon = "../" + room.meta.split(" ")[2];
-        var id = room.id;
-        VueInstance.addItem(name, "", icon, id);
+        api.room.getDevices(room.id)
+          .done(function(data, textStatus, jqXHR) {
+            var name = room.name;
+            var icon = "../" + room.meta.split(" ")[2];
+            var id = room.id;
+            VueInstance.addItem(name, "DEVICES: " + data.devices.length , icon, id);
+          })
+          .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.responseText);
+          })
       })
     })
     .fail(function(jqXHR, textStatus, errorThrown) {

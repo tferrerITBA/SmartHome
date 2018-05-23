01357c5c786f4f7dbace57311bfd1cbfee2d1bb0
gprinc <gprinc@itba.edu.ar>
2018-05-23 00:18:14 -0300

    adding ids

--- components.js
+++ components.js
@@ -23,25 +23,25 @@ template:
     </div>`
 })
 
-Vue.component('modal', {props: [], 
-	template: 
+Vue.component('modal', {props: [],
+	template:
 	`<transition name="modal">
 		<div class="modal-mask">
 		  <div class="modal-wrapper">
 			<div class="modal-container">
-	
+
 			  <div class="modal-header">
 				<slot name="header">
 				  default header
 				</slot>
 			  </div>
-	
+
 			  <div class="modal-body">
 				<slot name="body">
 				  default body
 				</slot>
 			  </div>
-	
+
 			  <div class="modal-footer">
 				<slot name="footer">
 				  <button id="deleteDevice" class="modal-default-button" @click="$emit('delete')">
--- devicesComponents.js
+++ devicesComponents.js
@@ -1,21 +1,20 @@
 Vue.component('AirConditioner', {props: ['id'],
 template:
-	`
-  <div v-bind:id="id">
+	`<div>
     <div class="action">
-      <h3 class="action-text" id="on-status">Off</h3>
-      <label class="switch">
+      <h3 class="action-text" v-bind:id="'on-status' + id">Off</h3>
+      <label class="switch" v-bind:id="id">
           <input type="checkbox" id="on-switch"/>
           <span class="switch-slider"></span>
       </label>
     </div>
     <div class="action">
       <h3 class="action-text">Set temperature (<sup>o</sup>C)</h3>
-      <div class="counter">
+      <div class="counter" v-bind:id="id">
           <button class="counter-button" type="button" id="minus">
             <img class="counter-button-icon" src="../../assets/MinusButton.png" alt="increment"/>
           </button>
-        <input class="counter-input" name="quantity" value="25" readonly>
+        	<input v-bind:id="'temp' + id" class="counter-input" name="quantity" value="25" readonly>
           <button class="counter-button" type="button" id="plus">
             <img class="counter-button-icon" src="../../assets/PlusButton.png" alt="decrement"/>
           </button>
@@ -23,16 +22,16 @@ template:
     </div>
     <div class="action">
       <h3 class="action-text">Set mode</h3>
-      <form class="radio-selection">
-        <div class="radio-button">
+      <form class="radio-selection" v-bind:id="'mode' + id">
+        <div class="radio-button" v-bind:id="id">
           <input id="cool" type="radio" name="mode" value="cool">
           <p class="radio-button-text ac-align">Cool</p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="heat" type="radio" name="mode" value="heat">
           <p class="radio-button-text ac-align">Heat</p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="fan" type="radio" name="mode" value="fan">
           <p class="radio-button-text ac-align">Fan</p>
         </div>
@@ -40,24 +39,24 @@ template:
     </div>
     <div class="action">
       <h3 class="action-text">Set vertical swing</h3>
-      <form class="radio-selection">
-        <div class="radio-button">
+      <form class="radio-selection" v-bind:id="'vswing' + id">
+        <div class="radio-button" v-bind:id="id">
           <input id="vSwingauto" type="radio" name="v-swing" value="auto">
           <p class="radio-button-text ac-align">Auto</p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="vSwing22" type="radio" name="v-swing" value="22">
           <p class="radio-button-text ac-align">22<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="vSwing45" type="radio" name="v-swing" value="45">
           <p class="radio-button-text ac-align">45<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="vSwing67" type="radio" name="v-swing" value="67">
           <p class="radio-button-text ac-align">67<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="vSwing90" type="radio" name="v-swing" value="90">
           <p class="radio-button-text ac-align">90<sup>o</sup></p>
         </div>
@@ -65,28 +64,28 @@ template:
     </div>
     <div class="action">
       <h3 class="action-text">Set horizontal swing</h3>
-      <form class="radio-selection">
-        <div class="radio-button">
+      <form class="radio-selection" v-bind:id="'hswing' + id">
+        <div class="radio-button" v-bind:id="id">
           <input id="hSwingauto" type="radio" name="h-swing" value="auto">
           <p class="radio-button-text ac-align">Auto</p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="hSwing-90" type="radio" name="h-swing" value="-90">
           <p class="radio-button-text ac-align">-90<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="hSwing-45" type="radio" name="h-swing" value="-45">
           <p class="radio-button-text ac-align">-45<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="hSwing0" type="radio" name="h-swing" value="0">
           <p class="radio-button-text ac-align">0<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="hSwing45" type="radio" name="h-swing" value="45">
           <p class="radio-button-text ac-align">45<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="hSwing90" type="radio" name="h-swing" value="90">
           <p class="radio-button-text ac-align">90<sup>o</sup></p>
         </div>
@@ -94,82 +93,76 @@ template:
     </div>
     <div class="action">
       <h3 class="action-text">Set fan speed</h3>
-      <form class="radio-selection">
-        <div class="radio-button">
+      <form class="radio-selection" v-bind:id="'fanspeed' + id">
+        <div class="radio-button" v-bind:id="id">
           <input id="fanSpeedauto" type="radio" name="fan-speed" value="auto">
           <p class="radio-button-text ac-align">Auto</p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="fanSpeed25" type="radio" name="fan-speed" value="25">
           <p class="radio-button-text ac-align">25<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="fanSpeed50" type="radio" name="fan-speed" value="50">
           <p class="radio-button-text ac-align">50<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="fanSpeed75" type="radio" name="fan-speed" value="75">
           <p class="radio-button-text ac-align">75<sup>o</sup></p>
         </div>
-        <div class="radio-button">
+        <div class="radio-button" v-bind:id="id">
           <input id="fanSpeed100" type="radio" name="fan-speed" value="100">
           <p class="radio-button-text ac-align">100<sup>o</sup></p>
         </div>
       </form>
     </div>
-  </div>
-  `})
+  </div>`})
 
 
   Vue.component('Blinds', {props: ['id'],
   template:
-  	`
-  <div class="action" v-bind:id="id">
-    <h3 class="action-text" id="open-status">Closed</h3>
-    <label class="switch">
+  	`<div class="action">
+    <h3 class="action-text" v-bind:id="'open-status' + id">Closed</h3>
+    <label class="switch" v-bind:id="id">
         <input type="checkbox" id="open-switch">
         <span class="switch-slider"></span>
     </label>
-  </div>
-  `})
+  </div>`})
 
 
 Vue.component('Door', {props: ['id'],
 template:
-`
-<div v-bind:id="id">
+`<div>
   <div class="action">
-    <h3 class="action-text" id="open-status">Closed</h3>
-    <label class="switch">
+    <h3 class="action-text" v-bind:id="'open-status' + id">Closed</h3>
+    <label class="switch" v-bind:id="id">
         <input type="checkbox" id="open-switch">
         <span class="switch-slider"></span>
     </label>
   </div>
   <div class="action">
-    <h3 class="action-text" id="lock-status">Unlocked</h3>
-    <label class="switch">
+    <h3 class="action-text" v-bind:id="'lock-status' + id">Unlocked</h3>
+    <label class="switch" v-bind:id="id">
         <input type="checkbox" id="lock-switch">
         <span class="switch-slider"></span>
     </label>
   </div>
-</div>
-`})
+</div>`})
 
 
 Vue.component('Lamp', {props: ['id'],
 template:
-`
-<div v-bind:id="id">
+`<div v-bind:id="id">
   <div class="action">
-    <h3 class="action-text" id="on-status">Off</h3>
-    <label class="switch">
+    <h3 class="action-text" v-bind:id="'on-status' + id">Off</h3>
+    <label class="switch" v-bind:id="id">
         <input type="checkbox" id="on-switch"/>
         <span class="switch-slider"></span>
     </label>
   </div>
   <div class="action">
       <h3 class="action-text">Set color</h3>
-      <div class="color-selection">
+      <div class="color-selection" v-bind:id="id">
           <select id="color" class="color-select">
               <option class="select-option" value="#FFFFFF">White</option>
               <option class="select-option" value="#FFFF00">Yellow</option>
@@ -187,20 +180,18 @@ template:
       <h3 class="action-text">Set brightness</h3>
       <div class="brightness">
         <img class="small-icon" src="../../assets/BrightnessIcon.png"/>
-        <div class="slider-container">
+        <div class="slider-container" v-bind:id="id">
             <input type="range" min="0" max="100" value="100" class="slider" id="brightness"/>
         </div>
         <img class="large-icon" src="../../assets/BrightnessIcon.png"/>
       </div>
   </div>
-</div>
-`})
+</div>`})
 
 
 Vue.component('Oven', {props: ['id'],
 template:
-`
-<div v-bind:id="id">
+`<div v-bind:id="id">
   <div class="action">
     <h3 class="action-text" id="on-status">Off</h3>
     <label class="switch">
@@ -210,23 +201,23 @@ template:
   </div>
   <div class="action">
     <h3 class="action-text">Set temperature (<sup>o</sup>C)</h3>
-    <div class="oven-heat">
-              <input type="range" min="90" max="230" value="90" class="slider oven" id="temperature"/>
+    <div class="oven-heat" v-bind:id="id">
+      <input type="range" min="90" max="230" value="90" class="slider oven" id="temperature"/>
       <p class="oven-heat-text" id="heat-text"></p>
     </div>
   </div>
   <div class="action">
     <h3 class="action-text">Set heat</h3>
-    <form class="radio-selection">
-      <div class="radio-button">
+    <form class="radio-selection" v-bind:id="'heat' + id">
+      <div class="radio-button" v-bind:id="id">
         <input id="heatconventional" type="radio" name="heat" value="conventional">
         <p class="radio-button-text">Conventional</p>
       </div>
-         <div class="radio-button">
+         <div class="radio-button" v-bind:id="id">
            <input id="heatbottom" type="radio" name="heat" value="bottom">
               <p class="radio-button-text">Bottom</p>
          </div>
-         <div class="radio-button">
+         <div class="radio-button" v-bind:id="id">
            <input id="heattop" type="radio" name="heat" value="top">
            <p class="radio-button-text">Top</p>
          </div>
@@ -234,16 +225,16 @@ template:
   </div>
   <div class="action">
     <h3 class="action-text">Set grill</h3>
-    <form class="radio-selection">
-      <div class="radio-button">
+    <form class="radio-selection" v-bind:id="'grill' + id">
+      <div class="radio-button" v-bind:id="id">
         <input id="grilllarge" type="radio" name="grill" value="large">
         <p class="radio-button-text oven-align">Large</p>
       </div>
-       <div class="radio-button">
+       <div class="radio-button" v-bind:id="id">
          <input id="grilleco" type="radio" name="grill" value="eco">
         <p class="radio-button-text oven-align">Eco</p>
        </div>
-       <div class="radio-button">
+       <div class="radio-button" v-bind:id="id">
          <input id="grilloff" type="radio" name="grill" value="off">
         <p class="radio-button-text oven-align">Off</p>
       </div>
@@ -251,36 +242,34 @@ template:
   </div>
   <div class="action">
     <h3 class="action-text">Set convection</h3>
-    <form class="radio-selection">
-      <div class="radio-button">
+    <form class="radio-selection" v-bind:id="'conv' + id">
+      <div class="radio-button" v-bind:id="id">
         <input  id="convnormal" type="radio" name="convection" value="normal">
         <p class="radio-button-text oven-align">Normal</p>
       </div>
-     <div class="radio-button">
+     <div class="radio-button" v-bind:id="id">
        <input id="conveco" type="radio" name="convection" value="eco">
       <p class="radio-button-text oven-align">Eco</p>
      </div>
-     <div class="radio-button">
+     <div class="radio-button" v-bind:id="id">
        <input id="convoff" type="radio" name="convection" value="off">
       <p class="radio-button-text oven-align">Off</p>
      </div>
    </form>
   </div>
-</div>
-`})
+</div>`})
 
 
 Vue.component('Refrigerator', {props: ['id'],
 template:
-`
-<div v-bind:id="id">
+`<div v-bind:id="id">
   <div class="action">
     <h3 class="action-text">Set temperature (<sup>o</sup>C)</h3>
-    <div class="counter">
+    <div class="counter" v-bind:id="id">
         <button class="counter-button" type="button" id="minus">
           <img class="counter-button-icon" src="../../assets/MinusButton.png" alt="increment"/>
         </button>
-      <input class="counter-input" name="quantity" value="2" readonly/>
+      	<input v-bind:id="'temp' + id" class="counter-input" name="quantity" value="2" readonly/>
         <button class="counter-button" type="button" id="plus">
           <img class="counter-button-icon" src="../../assets/PlusButton.png" alt="decrement"/>
         </button>
@@ -288,11 +277,11 @@ template:
   </div>
   <div class="action">
     <h3 class="action-text">Set freezer temperature (<sup>o</sup>C)</h3>
-    <div class="counter">
+    <div class="counter" v-bind:id="id">
         <button class="counter-button" type="button" id="freezMinus">
           <img class="counter-button-icon" src="../../assets/MinusButton.png" alt="increment"/>
         </button>
-      <input class="counter-input" name="freez-quantity" value="-8" readonly/>
+      	<input v-bind:id="'freez' + id" class="counter-input" name="freez-quantity" value="-8" readonly/>
         <button class="counter-button" type="button" id="freezPlus">
           <img class="counter-button-icon" src="../../assets/PlusButton.png" alt="decrement"/>
         </button>
@@ -300,20 +289,19 @@ template:
   </div>
   <div class="action">
     <h3 class="action-text">Set mode</h3>
-    <form class="radio-selection">
-      <div class="radio-button">
+    <form class="radio-selection" v-bind:id="'mode' + id">
+      <div class="radio-button" v-bind:id="id">
         <input id="default" type="radio" name="mode" value="default"/>
         <p class="radio-button-text">Default</p>
       </div>
-      <div class="radio-button">
+      <div class="radio-button" v-bind:id="id">
         <input id="vacation" type="radio" name="mode" value="vacation"/>
         <p class="radio-button-text">Vacation</p>
       </div>
-      <div class="radio-button">
+      <div class="radio-button" v-bind:id="id">
         <input id="party" type="radio" name="mode" value="party"/>
         <p class="radio-button-text">Party</p>
       </div>
     </form>
   </div>
-</div>
-`})
+</div>`})
